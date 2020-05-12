6b31b4f7e49459599d7fd657a6ad0c19295a16b3
Dzmitry Babak <alexgott1990@gmail.com>
2020-05-12 22:15:28 +0300

    ref: del hw

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Brew />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Brew from './components/Brew.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Brew
   }
 }
 </script>
