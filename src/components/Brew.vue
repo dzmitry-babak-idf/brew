70b946a6569a03924d600a43222a4d69cc798426
Dzmitry Babak <alexgott1990@gmail.com>
2020-05-19 18:34:32 +0300

    add:show brew naming in list on page

--- src/components/Brew.vue
+++ src/components/Brew.vue
@@ -1,33 +1,59 @@
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-info">Breweries List</h1>
+<!--                <h2 class="text-center">{{msg}} {{name}}</h2>-->
+            </div>
+            <!-- /.col-12 -->
+        </div>
+        <div class="row">
 
+            <div class="col-6 brew-list">
+                <div class="card" style="width: 18rem;">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" v-for="brew in brews">{{brew.name}}, {{brew.state}}</li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6"><h2 class="text-center">right</h2></div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
 </template>
 
 <script>
+    import axios from 'axios'
+    //https://api.openbrewerydb.org/breweries
     export default {
         name: 'Brew',
-        props: {
-            msg: String
+        data: function(){
+            return{
+                brews:[]
+            }
+        },
+        mounted: function() {
+            axios.get('https://api.openbrewerydb.org/breweries')
+            .then((r)=>{
+                this.brews = r.data
+                console.log(r.data)
+            })
+            //https://api.openbrewerydb.org/breweries
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
-
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
+<style lang="scss" scoped>
+    .brew-list{
+        overflow-y: scroll;
+        height: 96vh;
+        li{
+            &:hover{
+                background-color: darkgrey;
+            }
+        }
     }
 </style>
