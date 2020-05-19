f0d1a94e41d425696b7f1e29cbf45ee8a6903597
Dzmitry Babak <alexgott1990@gmail.com>
2020-05-19 19:42:52 +0300

    ref:add List of brew in BrewList component

--- src/components/Brew.vue
+++ src/components/Brew.vue
@@ -3,19 +3,12 @@
         <div class="row">
             <div class="col-12">
                 <h1 class="text-center text-info">Breweries List</h1>
-<!--                <h2 class="text-center">{{msg}} {{name}}</h2>-->
             </div>
-            <!-- /.col-12 -->
         </div>
         <div class="row">
 
-            <div class="col-6 brew-list">
-                <div class="card" style="width: 18rem;">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item" v-for="brew in brews">{{brew.name}}, {{brew.state}}</li>
-
-                    </ul>
-                </div>
+            <div class="col-6">
+                <BrewList :brews="brews"/>
             </div>
             <div class="col-6"><h2 class="text-center">right</h2></div>
         </div>
@@ -26,20 +19,22 @@
 
 <script>
     import axios from 'axios'
+    import BrewList from "./BrewList";
     //https://api.openbrewerydb.org/breweries
     export default {
         name: 'Brew',
-        data: function(){
-            return{
-                brews:[]
+        components: {BrewList},
+        data: function () {
+            return {
+                brews: []
             }
         },
-        mounted: function() {
+        mounted: function () {
             axios.get('https://api.openbrewerydb.org/breweries')
-            .then((r)=>{
-                this.brews = r.data
-                console.log(r.data)
-            })
+                .then((r) => {
+                    this.brews = r.data
+                    console.log(r.data)
+                })
             //https://api.openbrewerydb.org/breweries
         }
     }
@@ -47,13 +42,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-    .brew-list{
-        overflow-y: scroll;
-        height: 96vh;
-        li{
-            &:hover{
-                background-color: darkgrey;
-            }
-        }
-    }
+
 </style>
---
+++ src/components/BrewList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card brew-list">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"
+                :key='index'
+                v-for="(brew, index) in brews"
+
+            >{{brew.name}}, {{brew.state}}
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BrewList",
+        props: {
+            brews: Array
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .brew-list {
+        overflow-y: scroll;
+        height: 93vh;
+
+        li {
+            &:hover {
+                background-color: darkgrey;
+            }
+        }
+    }
+</style>
